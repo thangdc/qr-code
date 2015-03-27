6b50f96425259bee78d51dff5c376d398749fc81
thangdc <thang@thangdc.com>
2015-03-27 13:24:57 +0700

    Init

---
+++ QRCodeGenerator/QRCodeGenerator/Main.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace QRCodeGenerator
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxData = new System.Windows.Forms.RichTextBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.btnEncode = new System.Windows.Forms.Button();
+            this.btnDecode = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbxData
+            // 
+            this.tbxData.Location = new System.Drawing.Point(12, 12);
+            this.tbxData.Name = "tbxData";
+            this.tbxData.Size = new System.Drawing.Size(227, 184);
+            this.tbxData.TabIndex = 0;
+            this.tbxData.Text = "";
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(264, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(177, 184);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // btnEncode
+            // 
+            this.btnEncode.Location = new System.Drawing.Point(12, 218);
+            this.btnEncode.Name = "btnEncode";
+            this.btnEncode.Size = new System.Drawing.Size(75, 23);
+            this.btnEncode.TabIndex = 2;
+            this.btnEncode.Text = "Mã Hóa";
+            this.btnEncode.UseVisualStyleBackColor = true;
+            this.btnEncode.Click += new System.EventHandler(this.btnEncode_Click);
+            // 
+            // btnDecode
+            // 
+            this.btnDecode.Location = new System.Drawing.Point(164, 218);
+            this.btnDecode.Name = "btnDecode";
+            this.btnDecode.Size = new System.Drawing.Size(75, 23);
+            this.btnDecode.TabIndex = 3;
+            this.btnDecode.Text = "Giải Mã";
+            this.btnDecode.UseVisualStyleBackColor = true;
+            this.btnDecode.Click += new System.EventHandler(this.btnDecode_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 253);
+            this.Controls.Add(this.btnDecode);
+            this.Controls.Add(this.btnEncode);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.tbxData);
+            this.Name = "Main";
+            this.Text = "Main";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox tbxData;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button btnEncode;
+        private System.Windows.Forms.Button btnDecode;
+    }
+}
+
---
+++ QRCodeGenerator/QRCodeGenerator/Main.cs
@@ -0,0 +1,89 @@
+﻿using ZXing;
+using ZXing.Common;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace QRCodeGenerator
+{
+    public partial class Main : Form
+    {
+        private readonly IBarcodeReader barcodeReader; 
+
+        public Main()
+        {
+            InitializeComponent();
+
+            barcodeReader = new BarcodeReader(); 
+        }
+
+        private void btnEncode_Click(object sender, System.EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbxData.Text))
+            {
+                MessageBox.Show("Vui lòng nhập nội dung cần mã hóa", 
+                    "QR Code Generator");
+                return;
+            }
+
+            var barcodeWriter = new BarcodeWriter
+            {
+                Format = BarcodeFormat.QR_CODE,
+                Options = new EncodingOptions
+                {
+                    Height = 150,
+                    Width = 150,
+                    Margin = 0
+                }
+            };
+
+            string content = tbxData.Text;
+
+            using (var bitmap = barcodeWriter.Write(content))
+            {
+                using (var stream = new MemoryStream())
+                {
+                    bitmap.Save(stream, ImageFormat.Png);
+                    var image = Image.FromStream(stream);
+                    pictureBox.Image = image;
+                }
+            }
+        }
+
+        private void btnDecode_Click(object sender, System.EventArgs e)
+        {
+            using (var openDlg = new OpenFileDialog())
+            {
+                openDlg.Multiselect = false;
+                if (openDlg.ShowDialog(this) == DialogResult.OK)
+                {
+                    string fileName = openDlg.FileName;
+                    if (!File.Exists(fileName))
+                    {
+                        return;
+                    }
+
+                    var image = (Bitmap)Bitmap.FromFile(fileName);
+                    pictureBox.Image = image;
+
+                    var result = barcodeReader.Decode(image);
+                    if (result == null)
+                    {
+                        result = barcodeReader.Decode(image);
+                    }
+
+                    if (result == null)
+                    {
+                        MessageBox.Show("Không nhận diện được mã QR Code",
+                        "QR Code Generator");
+                    }
+                    else
+                    {
+                        tbxData.Text = result.Text;
+                    }
+                }
+            }
+        }
+    }
+}
